41d02bd8daa2022ee1720f151448e5774b22495e
FuzzyWereBeast <32906753+FuzzyWereBeast@users.noreply.github.com>
2022-08-10 22:00:52 -0400

    Crafting sets update
    Turned on Crafting sets.
    Corrected Clothcraft body piece.

--- data/Fuzzy/Fuzzy_Crafting.lua
+++ data/Fuzzy/Fuzzy_Crafting.lua
@@ -2,7 +2,7 @@
 --Have a Normal/HQ/NQ state
 --Watch for gained buffs containing Imagery, if it does, jump to the correct Crafting Mode
 --Add Gathering Sets
---[[
+--
 sets.Fishing = {range="Ebisu Fishing Rod",neck="Fisher's Torque",body="Fisherman's Smock",hands="Fsh. Gloves",ring1="Noddy Ring",ring2=="Puffin Ring",waist="Fisher's Rope",legs="Fisherman's Hose",feet="Waders"}
 sets.Chocobo = {main="Chocobo Wand",head="Egg Helm",neck="Chocobo Torque",body="Blue Race Silks",hands="Chocobo Gloves",waist="Chocobo Rope",legs="Chocobo Hose",feet="Chocobo Boots"}
 sets.Chocobo.skill = {body="S. Blue Race Silks"}
@@ -11,24 +11,24 @@ sets.Chocobo.game = {body="Black Race Silks"}
 sets.crafting = {ring1="Craftkeeper's Ring",ring2="Artificer's Ring",back="Shaper's Shawl"}
 sets.crafting.HQ = {ring2="Craftmaster's Ring"}
 sets.crafting.Alchemy = {main="Caduceus",sub="Brewer's Escutcheon",neck="Alchemst. Torque",body="Alchemist's Smock"}
-sets.crafting.Alchemy.NQ = {ring1="Alchemist's Ring"}
+sets.crafting.Alchemy.NQ = set_combine(sets.crafting.Alchemy, {ring1="Alchemist's Ring"})
 sets.crafting.Leathercraft = {neck="Tanner's Torque",sub="Beveler's Escutcheon",body="Tanner's Smock"}
-sets.crafting.Leathercraft.NQ = {ring1="Tanner's Ring"}
+sets.crafting.Leathercraft.NQ = set_combine(sets.crafting.Leathercraft, {ring1="Tanner's Ring"})
 sets.crafting.Smithing = {neck="Smithy's Torque",sub="Smythe's Escutcheon",body="Blksmith. Smock"}
-sets.crafting.Smithing.NQ = {ring1="Smith's Ring"}
+sets.crafting.Smithing.NQ = set_combine(sets.crafting.Smithing, {ring1="Smith's Ring"})
 sets.crafting.Bonecraft = {neck="Boneworker's Torque",sub="Ossifier's Escutcheon",body="Bonewrk. Smock"}
-sets.crafting.Bonecraft.NQ = {ring1="Bonecrafter's Ring"}
+sets.crafting.Bonecraft.NQ = set_combine(sets.crafting.Bonecraft, {ring1="Bonecrafter's Ring"})
 sets.crafting.Cooking = {main="Hocho",sub="Chef's Escutcheon",neck="Culin. Torque",body="Culinarian's Smock"}
-sets.crafting.Cooking.NQ = {ring1="Chef's Ring"}
+sets.crafting.Cooking.NQ = set_combine(sets.crafting.Cooking, {ring1="Chef's Ring"})
 sets.crafting.Goldsmithing = {neck="Goldsm. Torque",sub="Toreutic Escutcheon",body="Goldsmith's Smock"}
-sets.crafting.Goldsmithing.NQ = {ring1="Goldsmith's Ring"}
-sets.crafting.Clothcraft = {neck="Weaver's Torque",sub="Plaiter's Escutcheon",body="Weaver's Smock"}
-sets.crafting.Clothcraft.NQ = {ring1="Tailor's Ring"}
+sets.crafting.Goldsmithing.NQ = set_combine(sets.crafting.Goldsmithing, {ring1="Goldsmith's Ring"})
+sets.crafting.Clothcraft = {head="Magnifying spectacles",neck="Weaver's Torque",sub="Plaiter's Escutcheon",body="Weaver's Apron"}
+sets.crafting.Clothcraft.NQ = set_combine(sets.crafting.Clothcraft, {ring1="Tailor's Ring"})
 sets.crafting.Woodworking = {neck="Carver's Torque",sub="Joiner's Escutcheon",body="Carpenter's Smock"}
-sets.crafting.Woodworking.NQ = {ring1="Carpenter's Ring"}
+sets.crafting.Woodworking.NQ = set_combine(sets.crafting.Woodworking, {ring1="Carpenter's Ring"})
 sets.crafting.Synergy = {}
 
---Specific items for crafting you may want to use organizer to collect.
+--[[ Specific items for crafting you may want to use organizer to collect.
 sets.items = {
 	item1="",
 	item2="",
